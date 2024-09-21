843446a6761152eae16cd8b7d03549e4696f5e92
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-09-22 01:08:31 +0900

    체크박스 과제 완료

--- JavaClass_kitri/servlet0919/WebContent/WEB-INF/views/index05.jsp
+++ JavaClass_kitri/servlet0919/WebContent/WEB-INF/views/index05.jsp
@@ -13,22 +13,22 @@
 		
 		<legend>상품 선택</legend>
 		<b>주문할 상품을 선택하세요</b><br/><br/>
-		<input type="checkbox" name="items" value="p3" ${checkTypeP3}/> 선물용 3kg
-		<input type="checkbox" name="items" value="p5" ${checkTypeP5}/> 선물용 5kg
-		<input type="checkbox" name="items" value="h3" ${checkTypeH3}/> 가정용 3kg
-		<input type="checkbox" name="items" value="h5" ${checkTypeH5}/> 가정용 5kg<br/><br/>
+		<input type="checkbox" name="items" value="p3" ${p3} ${saved}/> 선물용 3kg
+		<input type="checkbox" name="items" value="p5" ${p5} ${saved}/> 선물용 5kg
+		<input type="checkbox" name="items" value="h3" ${h3} ${saved}/> 가정용 3kg
+		<input type="checkbox" name="items" value="h5" ${h5} ${saved}/> 가정용 5kg<br/><br/>
 		<b>포장 선택</b><br/><br/>
-		<input type="radio" name="boxing" value="p1" ${selectedP1}/> 선물 포장
-		<input type="radio" name="boxing" value="p2" ${selectedP2} }/> 선물 포장 안함
+		<input type="radio" name="boxing" value="p1" ${p1} ${saved}/> 선물 포장
+		<input type="radio" name="boxing" value="p2" ${p2} ${saved}/> 선물 포장 안함
 	</fieldset>
 	
 	<fieldset>
 		<legend>배송 정보</legend>
 		<b>주문할 상품을 선택하세요</b><br/><br/>
-		이름: <input type="text" name="name" value=${name} ${readonly} required /> <br/>
-		배송주소: <input type="text" name="addr" value=${addr} ${readonly} required }/> <br/>
-		이메일: 이름: <input type="text" name="email" value={email} ${readonly} required/> <br/>
-		연락처: <input type="text" name="phone" value=${phone} ${readonly} required/> <br/>
+		이름: <input type="text" name="name" value="${name}" ${saved} /> <br/>
+		배송주소: <input type="text" name="addr" value="${addr}" ${saved} /> <br/>
+		이메일: <input type="text" name="email" value="${email}" ${saved}/> <br/>
+		연락처: <input type="text" name="phone" value="${phone}" ${saved}/> <br/>
 	</fieldset>
 	<input type="submit" value="전송" />
 	</form>
--- JavaClass_kitri/servlet0919/src/controller/formctrlTest.java
+++ JavaClass_kitri/servlet0919/src/controller/formctrlTest.java
@@ -1,6 +1,11 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,113 +27,52 @@ public class formctrlTest extends HttpServlet{
 			new View("/WEB-INF/views/login.jsp").render(req, resp);
 			return;
 		}
-		String[] checkedItem = (String[])session.getAttribute("checkedItems");
-		if( checkedItem != null) {
-			for(String s : checkedItem) {
-				System.out.println(s);
-				switch(s) {
-				case "p3":
-					req.setAttribute("checkTypeP3", "checked");
-					break;
-				case "p5":
-					req.setAttribute("checkTypeP5", "checked");
-					break;
-				case "h3":
-					req.setAttribute("checkTypeH3", "checked");
-					break;
-				case "h5":
-					req.setAttribute("checkTypeH5", "checked");
-					break;
-				}
-			}
-			
-		}
-		req.setCharacterEncoding("UTF-8");
-		resp.setCharacterEncoding("UTF-8");
-		resp.setContentType("text/html");
-		new View("/WEB-INF/views/index05.jsp").render(req, resp);
 		
+		if(session.getAttribute("saved") != null ){
+			req.setAttribute("saved","disabled");
+			@SuppressWarnings("unchecked")
+			Map<String, String> saved = (Map<String, String>)session.getAttribute("saved");
+			for(String s : saved.keySet()) {
+				req.setAttribute(s, saved.get(s));
+			}
+		}
+		
+		new View("/WEB-INF/views/index05.jsp").render(req, resp);
 	}
+	
 	
 	@Override
-	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String[] checkitems = req.getParameterValues("items");
+		req.setCharacterEncoding("UTF-8");
+		String[] orderItem = req.getParameterValues("items");
+		String boxingType = req.getParameter("boxing");
+		String name = req.getParameter("name");
+		String addr = req.getParameter("addr");
+		String email = req.getParameter("email");
+		String phone = req.getParameter("phone");
 		
-		HttpSession session = req.getSession();
-		session.setAttribute("checkedItems", checkitems);
+		if(orderItem != null && boxingType != null & name != null & addr != null & phone != null) {
+			Map<String, String> saved = new HashMap<>();
+			
+			for(String s : orderItem) {
+				saved.put(s, "checked");
+			}
+			saved.put(boxingType, "checked");
+			saved.put("name", name);
+			saved.put("addr", addr);
+			saved.put("email", email);
+			saved.put("phone", phone);
+			
+			HttpSession session = req.getSession();
+			session.setAttribute("saved", saved);
+			
+		} else {
+			
+		}
+		
 		new View("/servlet0919/form", ViewMethod.REDIRECT).render(req, resp);
-		SaveForm saveform = new SaveForm();
+
 		
 	}
-	
-	private class SaveForm{
-		private boolean p3;
-		private boolean p4;
-		private boolean h3;
-		private boolean h5;
-		private int select;
-		private String name;
-		private String addr;
-		private String email;
-		private String phone;
-		public boolean isP3() {
-			return p3;
-		}
-		public void setP3(boolean p3) {
-			this.p3 = p3;
-		}
-		public boolean isP4() {
-			return p4;
-		}
-		public void setP4(boolean p4) {
-			this.p4 = p4;
-		}
-		public boolean isH3() {
-			return h3;
-		}
-		public void setH3(boolean h3) {
-			this.h3 = h3;
-		}
-		public boolean isH5() {
-			return h5;
-		}
-		public void setH5(boolean h5) {
-			this.h5 = h5;
-		}
-		public int getSelect() {
-			return select;
-		}
-		public void setSelect(int select) {
-			this.select = select;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getAddr() {
-			return addr;
-		}
-		public void setAddr(String addr) {
-			this.addr = addr;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		public String getPhone() {
-			return phone;
-		}
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
-		
-		
-	}
-	
 	
 }
