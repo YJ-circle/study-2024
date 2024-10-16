9ecf4bc89b9b02f4b8dcfea124990593c66f7428
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-10-16 21:24:45 +0900

    스프링 입문 세션3: API - 세션3 완강

--- JavaClass_yh/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ JavaClass_yh/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,5 +22,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;// 문자가 그대로 내려간다. html, 템플릿 X
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // 객체를 반환하면 json으로 반환된다.
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
