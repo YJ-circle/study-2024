0a00213cc4fdf1b4d43dbc1485c171e7206ea5b5
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-10-16 21:09:23 +0900

    MVC와 템플릿

--- JavaClass_yh/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ JavaClass_yh/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,5 +15,11 @@ public class HelloController {
         //스프링 부트 기본 설정이
         // 뷰 리졸버(viewResolver)가 templeates의 hello를 먼저 찾게 된다.
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
