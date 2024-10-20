706a60b4070b73aa528418335539a89518ad6ab4
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-10-21 08:07:02 +0900

    고급1편: 스레드 만들기 - Thread 상속

---
+++ JavaClass_yh/java-ad-1/src/thread/start/HelloThreadMain.java
@@ -0,0 +1,11 @@
+package thread.start;
+
+public class HelloThreadMain {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        HelloThread helloThread = new HelloThread();
+        helloThread.start();
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+}
