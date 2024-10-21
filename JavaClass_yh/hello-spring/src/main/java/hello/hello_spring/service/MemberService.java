82cc011d728e4ba931058806fc5508855383b294
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-10-21 23:16:39 +0900

    스프링 입문 - 회원서비스 개발과 테스트

---
+++ JavaClass_yh/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        //증복회원 검증
+        vaildateDuplicatMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void vaildateDuplicatMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 있는 회원 입니다");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findBtId(memberId);
+    }
+}
