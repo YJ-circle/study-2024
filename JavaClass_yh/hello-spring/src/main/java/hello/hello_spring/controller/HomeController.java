0a596e39443ae57ad17e61499b1fd83c71e18139
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-10-23 04:03:08 +0900

    스프링 입문: 회원 웹 기능 - 등록

---
+++ JavaClass_yh/hello-spring/src/main/java/hello/hello_spring/controller/HomeController.java
@@ -0,0 +1,33 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    private MemberService memberService;
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/member/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
