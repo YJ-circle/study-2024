4d83dbcedb573d2609a0b51ebbf6cc7ad07b5f61
새림 <147745679+YJ-circle@users.noreply.github.com>
2024-11-01 13:12:35 +0900

    2024-11-01 Object 다형성 활용 방법 예제

---
+++ JavaClass_kitri/objectapplicable/src/objectapplicable/entity/TestEntity1.java
@@ -0,0 +1,25 @@
+package objectapplicable.entity;
+
+public class TestEntity1 {
+	private int a;
+	private String b;
+	
+	public TestEntity1() {}
+	public TestEntity1(int a, String b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+}
